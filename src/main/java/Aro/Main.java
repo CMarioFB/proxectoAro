0ed10daf4965cc2b1cf57a7400fa28ec8a44e149
21mma <21mma@LAPTOP-BAPOBPRG>
2022-03-08 06:39:41 +0100

    Renomeo o nome do proxecto a proxectoAro cambiando tamén o nome do cartafol que o contén. Renomeo a clase Circulo por Aro fixándote en renomear a clase de probas tamén e os comentarios.

--- src/main/java/Aro/Main.java
+++ src/main/java/Aro/Main.java
@@ -1,14 +1,14 @@
-package circulo;
+package Aro;
 
 import java.text.DecimalFormat;
 
 /**
- * Pruebas de refactorización en NetBeans con la clase Circulo
+ * Pruebas de refactorización en NetBeans con la clase Aro
  * @author A21CarlosFB
  */
 public class Main {
     public static void main(String[] args) {
-        Circulo circulo = new Circulo(37,43,2.5);
+        Aro circulo = new Aro(37,43,2.5);
         String saida =
                 "A coordenada X é "+circulo.obterX()+
                 "\nA coordenada Y é "+circulo.obterY()+
